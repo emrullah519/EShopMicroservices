d1c8775aff3ed80d8f45297dd85ef65fb0ba05f3
emrullah519 <emrullah3460@gmail.com>
2025-03-19 09:28:01 +0300

    domain models comp

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> Events => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            var dequeedEvents = _domainEvents.ToArray();
+            _domainEvents.Clear();
+            return dequeedEvents;
+        }
+    }
+}
